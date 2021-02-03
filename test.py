d8fc67c42496e84631a0057443464835c8618a5c
jcmayoral <jose.carlos.mayoral.banos@nmbu.no>
2021-02-03 14:59:47 +0100

    depth_evaluate

--- test.py
+++ test.py
@@ -59,6 +59,45 @@ def evaluate(model, path, iou_thres, conf_thres, nms_thres, img_size, batch_size
     return precision, recall, AP, f1, ap_class
 
 
+
+def evaluate_depth(model, path, iou_thres, conf_thres, nms_thres, img_size, batch_size):
+    model.eval()
+
+    # Get dataloader
+    dataset = MyListDataset(path, img_size=img_size, augment=False, multiscale=False)
+    dataloader = torch.utils.data.DataLoader(
+        dataset, batch_size=batch_size, shuffle=False, num_workers=1, collate_fn=dataset.collate_fn
+    )
+
+    Tensor = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor
+
+    labels = []
+    sample_metrics = []  # List of tuples (TP, confs, pred)
+    #print (len(dataloader))
+    #print(path)
+    for batch_i, (_, imgs, targets) in enumerate(tqdm.tqdm(dataloader, desc="Detecting objects")):
+
+        # Extract labels
+        labels += targets[:, 1].tolist()
+        # Rescale target
+        targets[:, 2:] = xywh2xyxy(targets[:, 2:])
+        targets[:, 2:] *= img_size
+
+        imgs = Variable(imgs.type(Tensor), requires_grad=False)
+
+        with torch.no_grad():
+            outputs = model(imgs)
+            outputs = non_max_suppression(outputs, conf_thres=conf_thres, nms_thres=nms_thres)
+        sample_metrics += get_batch_statistics(outputs, targets, iou_threshold=iou_thres)
+
+    # Concatenate sample statistics
+    if len(sample_metrics) == 0:  # no detections over whole validation set.
+        return None
+    true_positives, pred_scores, pred_labels = [np.concatenate(x, 0) for x in list(zip(*sample_metrics))]
+    precision, recall, AP, f1, ap_class = ap_per_class(true_positives, pred_scores, pred_labels, labels)
+
+    return precision, recall, AP, f1, ap_class
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--batch_size", type=int, default=8, help="size of each image batch")
